7881946919043a9cb90330ba682004a6aa4ab7b5
Jeff Yates <jeff@khanacademy.org>
2026-01-14 19:59:12 -0600

    Add a test

---
+++ src/__tests__/x-impl.test.ts
@@ -0,0 +1,15 @@
+import {describe, expect, it, vi} from "vitest";
+import { xImpl } from "../x-impl";
+
+describe("xImpl", () => {
+    it("should log 'Hello, world!'", () => {
+        // Arrange
+        const consoleSpy = vi.spyOn(console, "log");
+
+        // Act
+        xImpl();
+
+        // Assert
+        expect(consoleSpy).toHaveBeenCalledWith("Hello, world!");
+    });
+});
